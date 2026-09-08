5b66f9dc30c7374e6a2d114e682e95e0bc7e2277
cristian13nr-alt <cristian13nr@gmail.com>
2026-09-08 14:15:12 -0500

    Fix duplicate import of updateThirdParty function

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import {
   fetchAccounts, insertAccount, deleteAccount,
   fetchEntries, insertEntry,
   fetchInvoices, insertInvoice, markInvoicePosted, fetchVoucherTypes, insertVoucherType, deleteVoucherType,
-  fetchThirdParties, insertThirdParty, updateThirdParty, deleteThirdParty, updateThirdParty
+  fetchThirdParties, insertThirdParty, updateThirdParty, deleteThirdParty,
   fetchPurchases, insertPurchase, markPurchasePosted,
 } from "./lib/db";
 
